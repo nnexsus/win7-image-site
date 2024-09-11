2603922b2fc8f65ea3e8a330eeb63ae90de97eda
nnexsus <nexsusacc@gmail.com>
2024-09-11 10:54:28 -0500

    test3

--- netlify/functions/image.mjs
+++ netlify/functions/image.mjs
@@ -1,9 +1,13 @@
 export default async (req, context) => {
     const { id } = context.params;
 
-    return(`<img width="100%" src="https://arina.lol/api/win7/acfile/${id}"/> <h1>${ id }</h1>`)
+    const body = new ReadableStream({
+        start(controller) {
+          controller.enqueue(encoder.encode(`<img width="100%" src="https://arina.lol/api/win7/acfile/${id}"/> <h1>${ id }</h1>`));
+        }
+      });
 
-    //return new Response(`<img width="100%" src="https://arina.lol/api/win7/acfile/${id}"/> <h1>${ id }</h1>`);
+    return new Response(body);
 };
 
 export const config = {
