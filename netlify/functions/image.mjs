87a4a232e2b3f724adff39e2f37a5400eef553f0
nnexsus <nexsusacc@gmail.com>
2024-09-10 21:27:23 -0500

    test3

--- netlify/functions/image.mjs
+++ netlify/functions/image.mjs
@@ -1,9 +1,9 @@
 export default async (req, context) => {
     const { id } = context.params;
 
-    return new Response(`${ id }`);
+    return new Response(`<img width="100%" src="https://arina.lol/api/win7/acfile/${id}"/> <h1>${ id }</h1>`);
 };
 
 export const config = {
     path: "/image/:id"
-};
+};  
